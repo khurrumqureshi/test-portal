f46c6364e77a4f4016a677fc38d31c64ae50eb05
ahsankhan1911 <ahsankhan1911@gmail.com>
2017-05-31 15:14:29 +0500

    Search query work done

--- api/airports/airports.controller.js
+++ api/airports/airports.controller.js
@@ -2,26 +2,47 @@ const mongoose = require('mongoose');
 const Airports = mongoose.model('airports');
 const boom = require('boom');
 
-exports.search =(req, res, next) => {
+exports.search = (req, res, next) => {
+
+    let search = req.query.data;
+
+    if (search.length <= 2) {
+        next(boom.badImplementation("Please provide atleast 3 letters to search"));
+    } else {
+
+        Airports.find({name: {$regex: search,$options: 'i'}})
+
+            .then((doc) => {
+                if (doc.length === 0) {
+
+                    next(boom.badImplementation("no such airport with keyword " + search + " found"));
+                }
+                else {
+                res.send(doc);
+                }
+            })
+            .catch((err) => {
+
+                next(boom.badImplementation(err.toString()));
+
+            });
+
+    }
+
+};
 
 
-}
+exports.createAirPort = (req, res, next) => {
 
+    let NewAirport = new Airports(req.body);
 
+    NewAirport.save ((err, airport) => {
+        if (err) {
+           next(boom.unauthorized(err.toString()));
+        }
 
+        else {
+            res.json(airport);
 
-
-// exports.createAirPort = (req, res, next) => {
-
-//     let NewAirport = new Airports(req.body);
-
-//     NewAirport.save ((err, airport) => {
-//         if (err) {
-//            next(boom.unauthorized(err.toString()));
-//         }
-
-//         else {
-//             res.json(airport);
-
-//         }
-//     })};
+        }
+    })};
