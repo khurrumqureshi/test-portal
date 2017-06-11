abed9ecc37e48b2dae0b83d19875b5d1e48a370f
Ahsan Ali Khan <Ahsan Ali Khan>
2017-06-12 02:03:37 +0500

    incomplete

--- api/airports/airports.controller.js
+++ api/airports/airports.controller.js
@@ -7,24 +7,47 @@ let data = require('../../flight_data.json');
 exports.searchfilter = (req, res, next) => {
 
 
-let reqdata1 = req.query.statusText;
-let reqdata2 = req.query.contactEmail;
-let reqdata3 = req.query.paymentStatusText;
-let reqdata4 = req.query.paymentPaymentMethod;
-let reqdata5 = req.query.paymentType;
-let reqdata6 = new Date(req.query.from);
-let reqdata7 = new Date(req.query.to);
+let reqdata1 = req.query.input;
+// let reqdata2 = req.query.contactEmail;
+// let reqdata3 = req.query.paymentStatusText;
+// let reqdata4 = req.query.paymentPaymentMethod;
+// let reqdata5 = req.query.paymentType;
+// let reqdata6 = new Date(req.query.from);
+// let reqdata7 = new Date(req.query.to);
 
  let filterData = data.filter((el) => {
     
-     return el.paymentStatusText  == reqdata3 &&     
-      el.contact.email == reqdata2 &&
-      el.statusText == reqdata1;
-
-     
- });
     
-  res.send(filterData)
+    switch (reqdata1)
+   {
+       case el.contact.email: 
+           
+          reqdata1 == el.contact.email;
+          return;
+
+          default:
+          console.log("Nothing baby");
+         
+     }
+    
 
 
+   });
+
+  //   switch (reqdata1)
+  //  {
+  //      case statusText:
+  //      res.send("hello")
+  //      break;
+  //      case "Hi":
+  //      res.send("Hi");
+  //      break;
+
+  //      default:
+
+  //      res.send("Bye")
+         
+  //    }
+
+ res.send(filterData)
 }
