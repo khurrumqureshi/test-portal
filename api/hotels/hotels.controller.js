75763635a808f0cf9fd9b63b2911200c8b2778c0
saad khan <saad khan>
2017-06-16 02:00:16 -0700

    near

--- api/hotels/hotels.controller.js
+++ api/hotels/hotels.controller.js
@@ -7,15 +7,29 @@ let _ = require('lodash');
 exports.searchfilter = (req, res, next) => {
 
     var map = req.query;
-    var shouldFilter = !map.paymentMethod && !map.paymentStatusText;
+    var shouldFilter = !map.paymentMethod && 
+    !map.paymentStatusText && 
+    !map.statusText && 
+    !map.email &&
+    !map.type &&
+    !map.createdAtto && !map.createdAtfrom
+    ;
 
     let newItems = [];
     if(!shouldFilter){
-        data.map(hotel => {
+        data.filter(hotel => {
             hotel._paymentMethod = !map.paymentMethod || _.toLower(map.paymentMethod) == _.toLower(hotel.payment.paymentMethod);
             hotel._paymentStatusText = !map.paymentStatusText || _.toLower(map.paymentStatusText) == _.toLower(hotel.paymentStatusText);
+            hotel._statusText = !map.statusText || _.toLower(map.statusText) == _.toLower(hotel.statusText);
+            hotel._email = !map.email || _.toLower(map.email) == _.toLower(hotel.contact.email);
+            hotel._type = !map.type || _.toLower(map.type) == _.toLower(hotel.payment.type);
+            hotel._createdAt = !map.createdAtto && !map.createdAtfrom || map.createdAtto && map.createdAtfrom
 
-            hotel._visible = hotel._paymentMethod && hotel._paymentStatusText;
+            hotel._visible = hotel._paymentMethod && 
+            hotel._paymentStatusText &&  
+            hotel._statusText &&  
+            hotel._email &&
+            hotel._type ;
             if(hotel._visible) newItems.push(hotel);
         });
 
