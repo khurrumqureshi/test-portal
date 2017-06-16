59031b0dbb9430bea5583a9fe787ce76e412292f
Ahsan Ali Khan <Ahsan Ali Khan>
2017-06-17 02:34:37 +0500

    filteraton completed

--- api/airports/airports.controller.js
+++ api/airports/airports.controller.js
@@ -6,48 +6,44 @@ let data = require('../../flight_data.json');
 
 exports.searchfilter = (req, res, next) => {
 
+let data = require("../../flight_data.json");
+let _ = require('lodash');
 
-let reqdata1 = req.query.input;
-// let reqdata2 = req.query.contactEmail;
-// let reqdata3 = req.query.paymentStatusText;
-// let reqdata4 = req.query.paymentPaymentMethod;
-// let reqdata5 = req.query.paymentType;
-// let reqdata6 = new Date(req.query.from);
-// let reqdata7 = new Date(req.query.to);
+exports.searchfilter = (req, res, next) => {
 
- let filterData = data.filter((el) => {
-    
-    
-    switch (reqdata1)
-   {
-       case el.contact.email: 
-           
-          reqdata1 == el.contact.email;
-          return;
+    var map = req.query;
+    var shouldFilter = !map.paymentMethod && 
+    !map.paymentStatusText && 
+    !map.statusText && 
+    !map.email &&
+    !map.type &&
+    !map.createdAtfrom && !map.createdAtto
+    ;
 
-          default:
-          console.log("Nothing baby");
-         
-     }
-    
+    let newItems = [];
+    if(!shouldFilter){
+        data.filter(airport => {
+            airport._paymentMethod = !map.paymentMethod || _.toLower(map.paymentMethod) == _.toLower(airport.payment.paymentMethod);
+            airport._paymentStatusText = !map.paymentStatusText || _.toLower(map.paymentStatusText) == _.toLower(airport.paymentStatusText);
+            airport._statusText = !map.statusText || _.toLower(map.statusText) == _.toLower(airport.statusText);
+            airport._email = !map.email || _.toLower(map.email) == _.toLower(airport.contact.email);
+            airport._type = !map.type || _.toLower(map.type) == _.toLower(airport.payment.type);
+            airport._createdAt = !map.createdAtfrom && !map.createdAtto || airport.createdAt >= map.createdAtfrom && airport.createdAt <= map.createdAtto;
 
+            airport._visible = airport._paymentMethod && 
+            airport._paymentStatusText &&  
+            airport._statusText &&  
+            airport._email &&
+            airport._type &&
+            airport._createdAt ;
+            if(airport._visible) newItems.push(airport);
+        });
 
-   });
+        res.send(newItems);
+    }
+    else{
+        res.send(data);
+    }
+};
 
-  //   switch (reqdata1)
-  //  {
-  //      case statusText:
-  //      res.send("hello")
-  //      break;
-  //      case "Hi":
-  //      res.send("Hi");
-  //      break;
-
-  //      default:
-
-  //      res.send("Bye")
-         
-  //    }
-
- res.send(filterData)
 }
