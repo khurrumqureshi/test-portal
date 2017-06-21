7ede7d8aec24fe2350544444155245762668a848
saad khan <saad.khan6670@gmail.com>
2017-06-21 14:02:17 -0700

    resolved

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const hotels = require('./api/hotels/hotels.model');
 const port = 5000;
+
 
 //body parser middleware
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/api', require('./api'));
+
+app.listen(port, () => {
+    console.log(`Running server on ${port}`);
+});
