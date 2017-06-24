372a5210e634718b25c10020af0772e68892afd4
saad khan <saad.khan6670@gmail.com>
2017-06-24 14:24:47 -0700

    Hotel Sorting done

--- api/hotels/hotels.controller.js
+++ api/hotels/hotels.controller.js
@@ -1,26 +1,69 @@
 let data = require("../../hotel_data.json");
-    let _ = require('lodash');
+let _ = require('lodash');
 
-    exports.sorting = (req, res, next) => {
+exports.sorting = (req, res, next) => {
 
-    let  newItem = [];
+    let newItem = [];
     let sortInput = req.query;
 
-    if(sortInput.query == "total") {
-    let arrfound = _.sortBy(data, [(hotel) => {
-        if(sortInput.dir == "DESC") {
-            return -(hotel.totals.total);
-        }
-        else {
-        return hotel.totals.total;
-        }
-    }]);
-    
-    res.send(arrfound);
+    //************** Total Sorting ************
+    if (sortInput.query == "total") {
+        let arrfound = _.sortBy(data, [(hotel) => {
+            if (sortInput.dir == "DESC") {
+                return -(hotel.totals.total);
+            } else {
+                return hotel.totals.total;
+            }
+        }]);
+
+        res.send(arrfound);
     }
+    //************** starRating Sorting ************
+    if (sortInput.query == "rating") {
 
-    if(sortInput.query == "rating") {
-       console.log( _.sort(data, {products: [{options: { starRating: }}]}));
+        let arrfound;
 
-         
-    }}
+        data.forEach(function(hotel) {
+            hotel.products.forEach(function(hotel2) {
+
+                newItem.push(hotel2.options);
+
+            });
+        });
+        newItem.sort((a, b) => {
+            if (sortInput.dir == "DESC") {
+                return b.starRating - a.starRating;
+            } else {
+                return a.starRating - b.starRating;
+            }
+
+        });
+
+        res.send(newItem);
+
+    }
+    //************** firstCancellationDate Sorting ************
+
+    if (sortInput.query == "canceldate") {
+
+        let arrfound;
+
+        data.forEach(function(hotel) {
+            hotel.products.forEach(function(hotel2) {
+
+                newItem.push(hotel2.options);
+
+            });
+        });
+        newItem.sort((a, b) => {
+            if (sortInput.dir == "DESC") {
+                return new Date(b.firstCancellationDate) - new Date(a.firstCancellationDate);
+            } else {
+                return new Date(a.firstCancellationDate) - new Date(b.firstCancellationDate);
+            }
+
+        });
+
+        res.send(newItem);
+    }
+}
