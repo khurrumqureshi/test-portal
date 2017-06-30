9f8197450e6da9792863942f301f90e6fd4d6a03
saad khan <saad.khan6670@gmail.com>
2017-06-30 15:26:45 -0700

    commit

--- api/hotels/hotels.controller.js
+++ api/hotels/hotels.controller.js
@@ -6,64 +6,32 @@ exports.sorting = (req, res, next) => {
     let newItem = [];
     let sortInput = req.query;
 
-    //************** Total Sorting ************
-    if (sortInput.query == "total") {
-        let arrfound = _.sortBy(data, [(hotel) => {
-            if (sortInput.dir == "DESC") {
-                return -(hotel.totals.total);
-            } else {
-                return hotel.totals.total;
-            }
-        }]);
-
-        res.send(arrfound);
+    if(req.query){
+         return res.send(data);
     }
-    //************** starRating Sorting ************
-    if (sortInput.query == "rating") {
 
-        let arrfound;
 
-        data.forEach(function(hotel) {
-            hotel.products.forEach(function(hotel2) {
 
-                newItem.push(hotel2.options);
+   data.sort((a, b) => {
+          
+ var productA = _.find(a.products, {type: 'hotel'});
+            var productB = _.find(b.products, {type: 'hotel'});
 
-            });
-        });
-        newItem.sort((a, b) => {
-            if (sortInput.dir == "DESC") {
-                return b.starRating - a.starRating;
-            } else {
-                return a.starRating - b.starRating;
-            }
-
-        });
-
-        res.send(newItem);
-
-    }
-    //************** firstCancellationDate Sorting ************
-
-    if (sortInput.query == "canceldate") {
-
-        let arrfound;
-
-        data.forEach(function(hotel) {
-            hotel.products.forEach(function(hotel2) {
-
-                newItem.push(hotel2.options);
-
-            });
-        });
-        newItem.sort((a, b) => {
-            if (sortInput.dir == "DESC") {
-                return new Date(b.firstCancellationDate) - new Date(a.firstCancellationDate);
+            if(req.query.starRating){
+if (sortInput.dir == "DESC") {
+                return new Date(productsA.options.firstCancellationDate) - new Date(a.firstCancellationDate);
             } else {
                 return new Date(a.firstCancellationDate) - new Date(b.firstCancellationDate);
             }
+        }
+        else if(req.query.firstCancellationDate){
 
-        });
+        }
+   });
 
-        res.send(newItem);
-    }
+        
+        
+
+    res.send(data);
+    
 }
