794f30f2bd1e8e3cacc894f3954915e770091a95
saad khan <saad.khan6670@gmail.com>
2017-07-06 13:04:03 -0700

    Work Started

--- api/hotels/hotels.controller.js
+++ api/hotels/hotels.controller.js
@@ -1,44 +1,19 @@
 let data = require("../../hotel_data.json");
+let userdata = require("../../user_data.json")
 let _ = require('lodash');
 
-exports.sorting = (req, res, next) => {
+exports.booking = (req, res, next) => {
 
-    let newItem = [];
-    let sortInput = req.query;
+    
 
-    if (sortInput) {
-        return res.send(data);
-    }
+    
 
-    let arr = _.sortBy(data, [(a) => {
+//     var newItem = [];
 
+//    let arrFound =  _.find(data , {name : "Dubai, Dubai Emirate, United Arab Emirates"})
+//     userdata.arrFound(arrFound);
+   
+//    console.log(arrFound);
 
-        var productA = _.find(a.products, {
-            type: 'hotel'
-        });
-
-        if (sortInput.query == "total") {
-            if (sortInput.dir == "desc") {
-                return -(a.totals.total);
-
-            }
-            return a.totals.total;
-        } else if (sortInput.query == "starrating") {
-            if (sortInput.dir == "desc") {
-                return -(productA.options.starRating);
-
-            }
-
-            return productA.options.starRating;
-        } else if (sortInput.query == "canceldate") {
-            if (sortInput.dir == "desc") {
-                return -(productA.options.firstCancellationDate);
-            }
-
-            return productA.options.firstCancellationDate;
-        }
-    }]);
-
-    res.send(arr);
 
 }
