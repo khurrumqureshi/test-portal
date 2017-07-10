fd6178acad815203c47ff8f8a1a73fa51e023c48
Ahsan Khan <ahsankhan1911@gmail.com>
2017-07-10 14:02:36 +0500

    Booking Completed

--- api/airports/airports.controller.js
+++ api/airports/airports.controller.js
@@ -10,7 +10,7 @@ exports.Booking = (req, res, next) => {
     let input = _.toInteger(req.query.id_airport);
     let arrFound = _.find(data, { id_airport: input });
     CustomerDetails._id = uuid();
-    
+
 
 
     if (!CustomerDetails.Firstname || !CustomerDetails.Lastname || !CustomerDetails.ContactNo || !CustomerDetails.NIC || !CustomerDetails.BookedFor) {
@@ -18,8 +18,8 @@ exports.Booking = (req, res, next) => {
         return res.send("Please fill out all the fields");
     }
     let AirportData = ({ "AirportId": arrFound.id_airport, "AirportName": arrFound.name });
-     
-      CustomerDetails.Flight = AirportData;
+
+    CustomerDetails.Flight = AirportData;
     CustomerDetails.BookedFor = new Date(CustomerDetails.BookedFor);
 
     booking_data.push(CustomerDetails);
