bd267de63f0c8be69780e9e066bbc3ed1247a14d
Ahsan Khan <ahsankhan1911@gmail.com>
2017-07-10 14:43:22 +0500

    ...

--- api/airports/index.js
+++ api/airports/index.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
-const airport = require('./airports.controller');
+const airport = require('./airport.controller');
 
 router.get('/sort', airport.sorting);
-router.post('/book', airports.Booking);
+router.post('/book', airport.Booking);
 module.exports = router;
