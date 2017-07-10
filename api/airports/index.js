55eef962185f9caaf87115420e972784805c83f7
Ahsan Khan <ahsankhan1911@gmail.com>
2017-07-10 14:44:40 +0500

    ...

--- api/airports/index.js
+++ api/airports/index.js
@@ -3,4 +3,5 @@ const airport = require('./airport.controller');
 
 router.get('/sort', airport.sorting);
 router.post('/book', airport.Booking);
+router.get('/search', airports.searchfilter);
 module.exports = router;
