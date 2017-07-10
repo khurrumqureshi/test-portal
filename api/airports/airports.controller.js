b5633a06cf6b8c38d0f9af3bf8d76b5de41d4a26
Ahsan Khan <ahsankhan1911@gmail.com>
2017-07-10 14:23:00 +0500

    ...

--- api/airports/airports.controller.js
+++ api/airports/airports.controller.js
@@ -7,41 +7,43 @@ let uuid = require("uuid/v4");
 
 
 
-exports.sorting = (req, res, next) => {
+exports.searchfilter = (req, res, next) => {
 
-    let sortInput = req.query;
+    var map = req.query;
+    var shouldFilter = !map.paymentMethod &&
+        !map.paymentStatusText &&
+        !map.statusText &&
+        !map.email &&
+        !map.type &&
+        !map.createdAtfrom && !map.createdAtto
+        ;
 
-    
-    let arr = _.sortBy(data, [(a) => {
+    let newItems = [];
+    if (!shouldFilter) {
+        data.filter(airport => {
+            airport._paymentMethod = !map.paymentMethod || _.toLower(map.paymentMethod) == _.toLower(airport.payment.paymentMethod);
+            airport._paymentStatusText = !map.paymentStatusText || _.toLower(map.paymentStatusText) == _.toLower(airport.paymentStatusText);
+            airport._statusText = !map.statusText || _.toLower(map.statusText) == _.toLower(airport.statusText);
+            airport._email = !map.email || _.toLower(map.email) == _.toLower(airport.contact.email);
+            airport._type = !map.type || _.toLower(map.type) == _.toLower(airport.payment.type);
+            airport._createdAt = !map.createdAtfrom && !map.createdAtto || airport.createdAt >= map.createdAtfrom && airport.createdAt <= map.createdAtto;
+
+            airport._visible = airport._paymentMethod &&
+                airport._paymentStatusText &&
+                airport._statusText &&
+                airport._email &&
+                airport._type &&
+                airport._createdAt;
+            if (airport._visible) newItems.push(airport);
+        });
+
+        res.send(newItems);
+    }
+    else {
+        res.send(data);
+    }
 
 
-        var productA = _.find(a.products);
-
-        if (sortInput.query == "total") {
-        
-            return a.totals.total;
-        } 
-        
-        else if (sortInput.query == "vendorStatus") {
-    
-        return productA.vendorStatus;
-        }
-        else if (sortInput.query == "officeId") {
-    
-
-            return productA.additionalData.officeId;
-        }
-    }]);
-
-        if (sortInput.dir == "desc") {
-            
-         arr.reverse();
-
-        } 
-
-   
-      res.send(arr);
-     
 }
 
 
